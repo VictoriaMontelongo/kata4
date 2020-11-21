10ad08a45c94728cf2067f177ffc6bce699bb54b
victo <victo@DESKTOP-VKVT4U5>
2020-11-21 17:19:51 +0000

    Versión2

---
+++ src/kata4/view/MailListReader.java
@@ -0,0 +1,32 @@
+
+package kata4.view;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import kata4.model.Mail;
+
+public class MailListReader {
+    public static List<Mail> read(String fileName){
+        List<Mail> list = new ArrayList<>();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+            IteratorReader iteratorReader = new IteratorReader(reader);
+            for (String line : iteratorReader) if(Mail.isMail(line)) list.add(new Mail(line));
+            reader.close();
+            return list;
+        }
+        catch(FileNotFoundException exception){
+            System.out.println("BufferedReader : File Not Found: " + exception);
+        }
+        catch(IOException exception){
+            System.out.println("ERROR MailListReader : IOEXCEPTION");
+        }
+        return list;
+    
+    }
+}
